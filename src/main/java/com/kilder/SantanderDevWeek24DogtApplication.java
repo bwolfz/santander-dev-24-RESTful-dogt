cc9c15ed3516047e1a326a57364c6e12c96e1679
bwolfz <kildertavora@gmail.com>
2024-07-26 21:12:06 -0300

    JAVA RESTful API for santander dev week

---
+++ src/main/java/com/kilder/SantanderDevWeek24DogtApplication.java
@@ -0,0 +1,13 @@
+package com.kilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek24DogtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek24DogtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kilder/SantanderDevWeek24DogtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek24DogtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
